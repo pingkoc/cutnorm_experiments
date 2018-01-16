7c654502bc817711b5caff472564084bd8658918
Ping-Ko Chiu <pingkoc@outlook.com>
2018-01-16 16:01:15 -0600

    changed norm_comparison to lovasz case 0.5

--- SBM/norm_comparison.py
+++ SBM/norm_comparison.py
@@ -2,49 +2,52 @@ import os
 import sys
 sys.path.append(os.getcwd() + '/../')
 from cutnorm import cutnorm
-from sbm import sbm, sbm_autoregressive, make_symmetric_triu
+from sbm import *
 from distort import add_gaussian_noise, shift
 import numpy as np
 import matplotlib.pyplot as plt
 
-prob_mat = [0.9, 0.2, 0.9, 0.4]
+# prob_mat = [0.9, 0.2, 0.9, 0.4]
+prob_mat = [0.5]
+n_com = len(prob_mat)
 
-# # Changing resolution
-# n_points = 8
-# logn_list = np.arange(n_points)+2
-# n_list = np.power(2, logn_list)
+# Changing resolution
+n_points = 12
+logn_list = np.arange(n_points)+2
+n_list = np.power(2, logn_list)
 
-# cutnorm_list = np.zeros(n_points)
-# l1_list = np.zeros(n_points)
-# l2_list = np.zeros(n_points)
-# linf_list = np.zeros(n_points)
+cutnorm_list = np.zeros(n_points)
+l1_list = np.zeros(n_points)
+l2_list = np.zeros(n_points)
+linf_list = np.zeros(n_points)
 
-# for i, n in enumerate(n_list):
-#     print("Computing norm for n=", n)
-#     A = sbm_autoregressive([n//4]*4, prob_mat)
-#     A = make_symmetric_triu(A)
-#     B = np.zeros((n, n))
-#     [perf, perf2, S, T, w] = cutnorm(A, B, max_round_iter=100)
-#     cutnorm_list[i] = perf2[0]
+for i, n in enumerate(n_list):
+    print("Computing norm for n=", n)
+    A = sbm_autoregressive([n//n_com]*n_com, prob_mat)
+    A = make_symmetric_triu(A)
+    B = sbm_autoregressive_prob([n//n_com]*n_com, prob_mat)
+    [perf, perf2, S, T, w] = cutnorm(A, B, max_round_iter=100)
+    cutnorm_list[i] = perf2[0]
 
-#     # L1 Norm
-#     l1_list[i] = np.linalg.norm(A, ord=1)
-#     l2_list[i] = np.linalg.norm(A, ord=2)
-#     linf_list[i] = np.linalg.norm(A, ord=np.inf)
+    # L1 Norm
+    C = ((A-B)/n**2).flatten()
+    l1_list[i] = np.linalg.norm(C, ord=1)
+    l2_list[i] = np.linalg.norm(C, ord=2)
+    linf_list[i] = np.linalg.norm(C, ord=np.inf)
 
-# print(l1_list)
-# print(l2_list)
-# print(linf_list)
-# print(cutnorm_list)
-# plt.plot(logn_list, cutnorm_list, label="Cutnorm")
-# plt.plot(logn_list, l1_list, label="L1")
-# plt.plot(logn_list, l2_list, label="L2")
-# plt.plot(logn_list, linf_list, label="Linf")
-# plt.xlabel("log2(n), n dim of matrix")
-# plt.ylabel("Norm")
-# plt.legend()
-# plt.title("Norms for SBMs of different resolution")
-# plt.show()
+print(l1_list)
+print(l2_list)
+print(linf_list)
+print(cutnorm_list)
+plt.plot(logn_list, cutnorm_list, label="Cutnorm")
+plt.plot(logn_list, l1_list, label="L1")
+plt.plot(logn_list, l2_list, label="L2")
+plt.plot(logn_list, linf_list, label="Linf")
+plt.xlabel("log2(n), n dim of matrix")
+plt.ylabel("Norm")
+plt.legend()
+plt.title("Norms for SBMs of different resolution")
+plt.show()
 
 # Shifting
 n = 1024
@@ -56,9 +59,9 @@ linf_list = np.zeros(n_points)
 roll_logn_list = np.arange(n_points)+2
 roll_list = np.power(2, roll_logn_list)
 
-A = sbm_autoregressive([n//4]*4, prob_mat)
+A = sbm_autoregressive([n//n_com]*n_com, prob_mat)
 A = make_symmetric_triu(A)
-B = np.zeros((n, n))
+B = sbm_autoregressive_prob([n//n_com]*n_com, prob_mat)
 
 for i, roll_val in enumerate(roll_list):
     print("Computing norm for roll_val=", roll_val)
@@ -67,9 +70,10 @@ for i, roll_val in enumerate(roll_list):
     cutnorm_list[i] = perf2[0]
 
     # L1 Norm
-    l1_list[i] = np.linalg.norm(rolled_A, ord=1)
-    l2_list[i] = np.linalg.norm(rolled_A, ord=2)
-    linf_list[i] = np.linalg.norm(rolled_A, ord=np.inf)
+    C = ((rolled_A - B)/n**2).flatten()
+    l1_list[i] = np.linalg.norm(C, ord=1)
+    l2_list[i] = np.linalg.norm(C, ord=2)
+    linf_list[i] = np.linalg.norm(C, ord=np.inf)
 
 print(l1_list)
 print(l2_list)
--- SBM/sbm.py
+++ SBM/sbm.py
@@ -26,11 +26,34 @@ def sbm(community_sizes, prob_mat):
 
     return sbm
 
-def sbm_autoregressive(community_sizes, prob_list):
-    # Construct the prob matrix from prob list using
-    # idea similar to that of autoregressive model
-    # Each element in the prob matrix is
-    # (p_i * p_j)^(abs(i - j))
+def sbm_prob(community_sizes, prob_mat):
+    community_sizes = np.array(community_sizes)
+    prob_mat = np.array(prob_mat)
+    if prob_mat.shape[0] != prob_mat.shape[1]:
+        raise ValueError("prob_mat needs to be square matrix.")
+    if not np.all(community_sizes > 0):
+        raise ValueError("Each community size in community_sizes "
+                         "needs to be greater than 1.")
+    if len(community_sizes) != len(prob_mat):
+        raise ValueError("community_sizes needs to be of size n if "
+                         "prob_mat is nxn")
+    if not (np.all(prob_mat >= 0) and np.all(prob_mat <= 1)):
+        raise ValueError("Needs to be a valid probability matrix.")
+
+    n = np.sum(community_sizes)
+    sbm = np.zeros((n, n))
+    for i, size_i in enumerate(community_sizes):
+        for j, size_j in enumerate(community_sizes):
+            prev_sum_i = np.sum(community_sizes[:i])
+            prev_sum_j = np.sum(community_sizes[:j])
+            prob = prob_mat[i][j]
+            prob_block = prob*np.ones((size_i, size_j))
+            sbm[prev_sum_i:prev_sum_i + size_i,
+                prev_sum_j:prev_sum_j + size_j] = prob_block
+
+    return sbm
+
+def _sbm_autoregressive_gen_prob_matrix(prob_list):
     n_probs = len(prob_list)
     prob_matrix = np.zeros((n_probs, n_probs))
     for i in range(n_probs):
@@ -39,7 +62,23 @@ def sbm_autoregressive(community_sizes, prob_list):
                 prob_matrix[i, i] = prob_list[i]
             else:
                 prob_matrix[i, j] = (prob_list[i] * prob_list[j])**(abs(i - j))
+    return prob_matrix
+
+def sbm_autoregressive(community_sizes, prob_list):
+    # Construct the prob matrix from prob list using
+    # idea similar to that of autoregressive model
+    # Each element in the prob matrix is
+    # (p_i * p_j)^(abs(i - j))
+    prob_matrix = _sbm_autoregressive_gen_prob_matrix(prob_list)
     return sbm(community_sizes, prob_matrix)
+
+def sbm_autoregressive_prob(community_sizes, prob_list):
+    # Construct the prob matrix from prob list using
+    # idea similar to that of autoregressive model
+    # Each element in the prob matrix is
+    # (p_i * p_j)^(abs(i - j))
+    prob_matrix = _sbm_autoregressive_gen_prob_matrix(prob_list)
+    return sbm_prob(community_sizes, prob_matrix)
 
 def make_symmetric_triu(mat):
     mat = np.triu(mat)
